5cd53c44cee4ff08a8501bef51b4128249185403
Joshua Hamilton <joshua@tinydog.io>
2023-01-18 20:38:27 -0500

    Serve files.

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String,
@@ -7,7 +8,12 @@ pub struct WebsiteHandler {
 
 impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
-        WebsiteHandler { public_path }
+        Self { public_path }
+    }
+
+    pub fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
     }
 }
 
@@ -15,8 +21,8 @@ impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello World</h1>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),

43ac9bb3816c867f5ca02f04b6f2ecdb6f8e2f7e
Joshua Hamilton <joshua@tinydog.io>
2023-01-18 19:45:06 -0500

    Route requests.

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,11 +1,18 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-       Response::new(StatusCode::Ok, Some("<h1>WebsiteHandler Response!!!</h1>".to_string())) 
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello World</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
 
